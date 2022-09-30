d9729fbf3c3dc7973e4db954656273104c3cd71d
And-Rom <you@example.com>
2022-10-01 00:35:34 +0300

    Task 6 EvenNum

---
+++ Task6_EvenNum/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+
+Console.WriteLine("ЗАДАЧА 6");
+Console.Write("Введите число: ");
+int checkNum = int.Parse(Console.ReadLine());
+if (checkNum % 2 == 0)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
