6e046aca21af83f2ce8f25cfa6a7bb6befc8df5b
And-Rom <you@example.com>
2022-10-01 01:43:21 +0300

    Task 8 OutputEvenNum

---
+++ Task8_OutputEvenNum/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 8
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("ЗАДАЧА 8");
+Console.Write("Введите число N: ");
+
+int n = int.Parse(Console.ReadLine());
+if (n == 1)
+{
+    Console.WriteLine("Четных чисел нет");
+}
+else
+{ 
+    int[] numbers = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        numbers[i] = i + 1;
+    }
+    string evenNumbers = "";
+    
+    for (int i = 0; i < n; i++)
+    {
+    if (numbers[i] % 2 == 0)
+        {
+            evenNumbers = evenNumbers + numbers[i].ToString() + ", "; 
+        }
+    }
+
+    evenNumbers = evenNumbers.Remove(evenNumbers.Length-2);
+    Console.WriteLine(evenNumbers);
+}
